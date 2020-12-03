d48fdc979d2ca345c18e6a880ac0010ff634e625
javierjavier <jrojasr94@live.cl>
2020-12-03 16:53:33 -0300

    fix Flask-SQLAlchemy to Docker Connection bug

--- SQLAlchemy/create.py
+++ SQLAlchemy/create.py
@@ -4,9 +4,13 @@ from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:test123@host.docker.internal:5432/postgres',
+)
+
 db.init_app(app)
-app.config["SLQALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:test123@host.docker.internal:5432/postgres'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 def main():
     db.create_all()
